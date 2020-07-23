bcc2ac11b604e8de693d6f1e4ae773070085b9e3
zc550110 <37893029+zc550110@users.noreply.github.com>
2020-07-23 16:33:34 +0800

    APP界面优化

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,28 +1,53 @@
 <template>
 	<view class="home">
-		<swiper class="swiper" 
-		:indicator-color="dotColor"
-		:indicator-active-color='activeColor'
-		:indicator-dots="indicatorDots"
-		:autoplay="autoplay" 
-		:interval="interval" 
-		:duration="duration">
+		<swiper class="banner-swiper" 
+		:indicator-color="banner.dotColor" 
+		:indicator-active-color='banner.activeColor'
+		:indicator-dots="banner.indicatorDots" 
+		:autoplay="banner.autoplay" 
+		:interval="banner.interval" 
+		:duration="banner.duration">
 			<swiper-item v-for="(item, index) in info" :key="index">
 				<view class="swiper-item">
 					<image :src="item" mode="scaleToFill" />
 				</view>
 			</swiper-item>
 		</swiper>
-		<!-- 幻灯片 -->	
-		<view class="nav">
-			<view class="nav-item" v-for="(item,i) in navList" :key="i">
-				<view @tap="skip(item)">
-					<text :class="['iconfont',item.icons]" :style="{'background-color':item.color}"></text>
-					<text class="nav-item-text">{{item.text}}</text>
+		<!-- swiper -->
+		<view class="common-menu">
+			<text class="title">常用菜单</text>
+			<view class="menu">
+				<view class="menu-item" v-for="(menu,i) in commonMenu" :key="i">
+					<view @tap="skip(menu)">
+						<text :class="['iconfont',menu.icons]" :style="{'background':menu.color}"></text>
+						<text class="menu-item-text">{{menu.text}}</text>
+					</view>
 				</view>
 			</view>
 		</view>
-		<!-- nav -->	
+		<!-- 常用菜单 -->
+		<view class="menu-box">
+		<text class="title">全部菜单</text>
+			<swiper class="menu-swiper" 
+			:indicator-color="menuSwiper.dotColor" 
+			:indicator-active-color='menuSwiper.activeColor'
+			:indicator-dots="menuSwiper.indicatorDots" 
+			:autoplay="menuSwiper.autoplay" 
+			:interval="menuSwiper.interval" 
+			:duration="menuSwiper.duration">
+				<swiper-item v-for='(menu,i) in menuList' :key='i'>
+					<view class="menu">
+						<view class="menu-item" v-for="(menuItem,j) in menu" :key="j">
+							<view @tap="skip(menuItem)">
+								<text :class="['iconfont',menuItem.icons]" :style="{'background':menuItem.color}"></text>
+								<text class="menu-item-text">{{menuItem.text}}</text>
+							</view>
+						</view>
+					</view>
+				</swiper-item>
+			</swiper>
+		</view>
+		<!-- nav -->
 		<view class="push-info">
 			<view class="push-info-item">
 				<view class="info-text">
@@ -49,75 +74,157 @@
 	</view>
 </template>
 <script>
-
 	export default {
 		name: 'Index',
 		data() {
 			return {
-				//幻灯片
-				indicatorDots: true,
-				autoplay: false,
-				interval: 2000,
-				duration: 500,
-				dotColor:'rgba(255,255,255,.6)',
-				activeColor:'#4ca2fb',				
+				//ad幻灯片
+				banner: {
+					indicatorDots: false,
+					autoplay: false,
+					interval: 2000,
+					duration: 500,
+					dotColor: 'rgba(255,255,255,.6)',
+					activeColor: '#4ca2fb',
+				},
 				info: [
-					 '/static/images/banner/banner1.jpg',
 					'/static/images/banner/banner1.jpg',
-					 '/static/images/banner/banner1.jpg'
 				],
 				//nav
-				navList: [{
+				menuSwiper: {
+					indicatorDots: true,
+					autoplay: false,
+					interval: 2000,
+					duration: 500,
+					dotColor: 'rgba(204,204,204,.6)',
+					activeColor: '#4ca2fb',
+				},
+				menu: [
+					{
 						icons: 'icon-produce',
 						text: '生产详情',
 						url: '/pages/product/product',
-						color: '#21c284'
+						color: 'linear-gradient(#fb635a, #e55a53)'
 					},
 					{
 						icons: 'icon-device',
 						text: '设备管理',
 						url: '/pages/device/device',
-						color: '#fb9f44'
-
+						color: 'linear-gradient(#f1b050, #e26a23)'
 					},
 					{
 						icons: 'icon-analyse',
 						text: '效力分析',
 						url: '/pages/analyse/analyse',
-						color: '#23bab5'
+						color: 'linear-gradient(#6bc3eb, #4996e6)'
 					},
 					{
 						icons: 'icon-file',
 						text: '工艺追溯',
 						url: '/pages/retrospect/retrospect',
-						color: '#00b7ee'
+						color: 'linear-gradient(#9bdc3e, #6db243)'
 					},
 					{
 						icons: 'icon-effict',
 						text: '效力管理',
 						url: '/pages/effect/effect',
-						color: '#5a9ef1'
+						color: 'linear-gradient(#f59d60, #ff7f45)'
 					},
 					{
 						icons: 'icon-quality',
 						text: '质量管理',
 						url: '/pages/quality/quality',
-						color: '#f9b823'
+						color: 'linear-gradient(#5ec5e6, #4996e8)'
 					},
 					{
 						icons: 'icon-search',
 						text: '相关查询',
 						url: '/pages/search/search',
-						color: '#9f68ff'
+						color: 'linear-gradient(#f4ca5b, #f3b64c)'
 					},
 					{
 						icons: 'icon-message',
 						text: '我的消息',
 						url: '/pages/info/info',
-						color: '#ed7240'
+						color: 'linear-gradient(#b76ada, #8e57d4)'
+					},
+					// 测试用
+				{
+					icons: 'icon-produce',
+					text: '生产详情',
+					url: '/pages/product/product',
+					color: 'linear-gradient(#fb635a, #e55a53)'
+				},
+				{
+					icons: 'icon-device',
+					text: '设备管理',
+					url: '/pages/device/device',
+					color: 'linear-gradient(#f1b050, #e26a23)'
+				
+				},
+				{
+					icons: 'icon-analyse',
+					text: '效力分析',
+					url: '/pages/analyse/analyse',
+					color: 'linear-gradient(#6bc3eb, #4996e6)'
+				},
+				{
+					icons: 'icon-file',
+					text: '工艺追溯',
+					url: '/pages/retrospect/retrospect',
+					color: 'linear-gradient(#9bdc3e, #6db243)'
+				},
+				{
+					icons: 'icon-effict',
+					text: '效力管理',
+					url: '/pages/effect/effect',
+					color: 'linear-gradient(#f59d60, #ff7f45)'
+				},
+				{
+					icons: 'icon-quality',
+					text: '质量管理',
+					url: '/pages/quality/quality',
+					color: 'linear-gradient(#5ec5e6, #4996e8)'
+				},
+				{
+					icons: 'icon-search',
+					text: '相关查询',
+					url: '/pages/search/search',
+					color: 'linear-gradient(#f4ca5b, #f3b64c)'
+				},
+				{
+					icons: 'icon-message',
+					text: '我的消息',
+					url: '/pages/info/info',
+					color: 'linear-gradient(#b76ada, #8e57d4)'
+				},
+				],
+				commonMenu: [
+					{
+						icons: 'icon-produce',
+						text: '生产详情',
+						url: '/pages/product/product',
+						color: 'linear-gradient(#fb635a, #e55a53)'
+					},
+					{
+						icons: 'icon-device',
+						text: '设备管理',
+						url: '/pages/device/device',
+						color: 'linear-gradient(#f1b050, #e26a23)',
 					}
 				],
-				spotVisible: false,
+				spotVisible:false
+			}
+		},
+		computed: {
+			menuList() {
+				let num = 1;
+				let size = 8;
+				let arr = [];
+				for (let i = 0; i < this.menu.length; i += size) {
+					arr.push(this.menu.slice(i, i + size));
+				}
+				return arr;
 			}
 		},
 		methods: {
@@ -141,58 +248,71 @@
 		}
 	}
 </script>
-
 <style lang="scss" scoped>
-	.swiper {
-		height: 400rpx;
+	.banner-swiper {
+		height: 350rpx;
 		.swiper-item {
 			display: block;
-			height: 400rpx;
-			line-height: 400rpx;
-			text-align: center;
+			height: 350rpx;
 			image {
 				width: 100%;
 				height: 100%;
 			}
 		}
-		
+	}    
+	// ad
+	.menu-box{
+		padding-bottom: 5px;
+		background: $white-color;		
 	}
-	/*菜单*/
-	.nav {
+	.common-menu{
+		margin-bottom: 20rpx;
+		background: $white-color;
+		}
+	// 常用菜单
+	.menu-swiper {
+		height: 370rpx;
+		background: $white-color;
+	}
+	// 菜单
+	.menu {
+		padding: 0 20rpx;
 		display: flex;
 		flex-wrap: wrap;
 		box-sizing: border-box;
-		padding: 40upx 0 0 0;
+		// padding: 20rpx 0 0 0;
 		background: $white-color;
-
-		.nav-item {
+		.menu-item {
 			box-sizing: border-box;
 			width: 25%;
-			padding-bottom: 40upx;
-
+			padding-bottom: 40rpx;
 			text {
 				display: block;
 				text-align: center;
-
 				// 文字
-				&.nav-item-text {
-					margin-top: 5upx;
-					color: $font-gray;
-					font-size: $font-24;
+				&.menu-item-text {
+					margin-top: 5rpx;
+					color:$font-text-color;
+					font-size:$font-24;
 				}
-
 				&.iconfont {
-					margin: 0 auto;
-					width: 95upx;
-					height: 95upx;
-					line-height: 95upx;
-					font-size: 50upx;
-					border-radius: 50%;
+					margin: 0 auto;					
+					width: 90rpx;
+					height: 90rpx;
+					line-height: 90rpx;
+					font-size: 45rpx;					
+					border-radius:50%;
 					background: $blue-color;
 					color: $white-color;
 
 				}
 			}
 		}
+	}
+    .title{
+		display: block;
+		padding: 20rpx 30rpx;
+		font-size:$font-36;
+		color:$font-text-color;		
 	}
 </style>
